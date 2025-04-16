97e1f3e8975fc3673b790a5c385af8aa949ef423
debugging404 <p.debugging404@gmail.com>
2025-04-16 23:20:00 +0700

    Add Course 1: Ch 1. Introduction

---
+++ 01-python/01-introduction.py
@@ -0,0 +1,37 @@
+print("Welcome to Fantasy Quest!")
+print("----------------------------------")
+
+print("Starting up game server...")
+print("local game server is listening on port 8080")
+print("----------------------------------")
+
+print("Loading character statistics")
+
+sword_damage = 10
+start_health = 100
+end_health = start_health + sword_damage
+
+print(f"Phu's health is: {start_health}")
+print(f"Phu takes {sword_damage} damage...")
+print(f"Phu's health is: {end_health}")
+
+
+print("----------------------------------")
+
+print("Greetings, adventurer!")
+
+print("----------------------------------")
+
+print("Jax: B-Kaw!")
+print("Hero: ...")
+print("Jax: Where are you off to this morning? Bkaw...")
+print("Hero: Where did an owl learn to speak??")
+
+print("----------------------------------")
+
+print("Fantasy Quest's innkeeper shop.")
+print("----------------------------------")
+
+print("Ah! Great choices...")
+print("Is there anything else I can help you with?")
+print("----------------------------------")
